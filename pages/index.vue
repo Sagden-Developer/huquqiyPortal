707ced2df9fe2c230102dcc958f827079af3c559
Xushnud9812 <hushnud1208@mail.ru>
2021-06-16 19:05:41 +0500

    sa

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <Slider />
+    <News />
+    <Section />
+    <Question />
+    <Rating />
+    <Ebook />
+    <Videos />
+    <Weblinks />
+  </div>
 </template>
 
 <script>
+import Slider from "../components/home/Slider";
+import News from "../components/home/News";
+import Section from "../components/home/Section";
+import Question from "../components/home/Question";
+import Rating from "../components/home/Rating";
+import Ebook from "../components/home/Ebook";
+import Videos from "../components/home/Videos";
+import Weblinks from "../components/home/Weblinks";
 export default {
   // name: "HuquqClientIndex",
 
-  // components: { ComponentName },
+  components: { Slider, News, Section, Question, Rating , Ebook, Videos ,Weblinks },
 
   // directives: { DirectiveName },
 
