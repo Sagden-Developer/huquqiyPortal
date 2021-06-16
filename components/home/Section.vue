0c229840a877f9d8f930b02e5e868e20ed0dc3a6
Erkinov Ulug'bek <erkinov997@gmail.com>
2021-06-17 04:19:08 +0500

    slide qoshildi

--- components/home/Section.vue
+++ components/home/Section.vue
@@ -1,15 +1,85 @@
 <template>
-  <div>
-      <h1>sections</h1>
+  <div id="sections">
+    <div class="sections-body">
+      <div class="section-text">{{ sectionTitle }}</div>
+      <div class="section-list-group">
+        <div
+          class="section-list-box"
+          v-for="(item, index) in sectionItem"
+          :key="index"
+        >
+          <img :src="item.sectionIcon" alt="" />{{ item.sectionTitle }}
+          <a :href="item.href" v-if="item.href">
+            Link
+          </a>
+          <nuxt-link :to="item.to" v-if="item.to">Link to </nuxt-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      sectionTitle: "Bo'limlar",
+      sectionItem: [
+        {
+          sectionIcon: require("@/assets/img/section/notariant.svg"),
+          sectionTitle: "Notariant",
+          to: "/hello"
+        },
+        {
+          sectionIcon: require("@/assets/img/section/advokat.svg"),
+          sectionTitle: "Advokatura",
+          to: "/hello"
+        },
+        {
+          sectionIcon: require("@/assets/img/section/yurudik.svg"),
+          sectionTitle: "Yurudik ta'lim",
+          href: "https://google.com"
+        },
+        {
+          sectionIcon: require("@/assets/img/section/lugat.svg"),
+          sectionTitle: "Yurudik lug'at",
+          href: "https://google.com"
+        },
+        {
+          sectionIcon: require("@/assets/img/section/dxa.svg"),
+          sectionTitle: "DXA",
+          href: "https://google.com"
+        },
+        {
+          sectionIcon: require("@/assets/img/section/advokat.svg"),
+          sectionTitle: "FXDYO",
+          href: "https://google.com"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+  #sections {
+    max-width: 1240px;
+    height: 529px;
+    border: 1px solid;
+    margin: 0 30px;
+    .section-list-group {
+      display: flex;
+      flex-wrap: wrap;
+      border: 1px solid blue;
+      justify-content: center;
+      .section-list-box {
+        width: 400px;
+        height: 222px;
+        border: 1px solid red;
+      }
+    }
+  }
+}
 </style>
--- components/home/Slider.vue
+++ components/home/Slider.vue
@@ -1,15 +1,97 @@
 <template>
   <div>
-      <h1>slider</h1>
+    <VueSlickCarousel v-bind="settings">
+      <div v-for="(item, index) in 5" :key="index" id="slider">
+        <div class="slide-texts">
+          <div class="head-text">
+            <h1>Huquqiy portal</h1>
+          </div>
+          <div class="info-text">
+            <h1>
+              O’zbekiston Respublikasi Milliy Huquqiy informatsion portali
+            </h1>
+          </div>
+        </div>
+        <div class="search-button">
+          <input type="text" v-model="power_from" :placeholder="placeholder" />
+        </div>
+        <div class="tag-text">Teglar</div>
+        <div class="tags">
+          <input type="button" class="tag-links" v-for="tagLink in tagLinks" :key="tagLink" :value="tagLink.title" />
+        </div>
+      </div>
+    </VueSlickCarousel>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      placeholder: "Izlash",
+      tagLinks: [
+        { title: "Advakatura" },
+        { title: "Notariant" },
+        { title: "Ishonch raqamlari" },
+        { title: "Onlayn Huquqiy maslahatlar" }
+      ],
+      settings: {
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToScroll: 1,
+        rtl: true
+      }
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+#slider {
+  max-width: 1240px;
+  height: 500px;
+  border: 1px solid;
+  border-radius: 24px;
+  padding: 55px;
+  margin: 0 auto;
+  background-image: url("/img/slide.png");
+  color: #fff;
+  .head-text {
+    text-transform: uppercase;
+  }
+  .info-text {
+    width: 60%;
+  }
+  .search-button {
+    & input[type=text] {
+      width: 570px;
+      height: 54px;
+      border-radius: 12px;
+      border: 2px solid;
+      outline: none;
+      margin: 10px 0 15px 0;
+      padding: 10px;
+    }
+  }
+  .tags {
+    padding: 10px 0 10px 0;
+    display: flex;
+    width: 35%;
+    flex-wrap: wrap;
+    justify-content: space-between;
 
+    .tag-links {
+      border: 1px solid #fff;
+      color: #fff;
+      border-radius: 24px;
+      margin: 10px 5px 5px 0;
+      padding: 5px 10px;
+      background: transparent;
+    }
+  }
+}
 </style>
