8163471925a32502eae3e56c3b04a993675be227
Erkinov Ulug'bek <erkinov997@gmail.com>
2021-06-22 18:35:01 +0500

    pull qilishga

--- components/home/Videos.vue
+++ components/home/Videos.vue
@@ -35,11 +35,24 @@
         </div>
       </Carousel>
       <div class="video-footer-text" v-text="footText"></div>
+      <CoolLightBox :items="items" :index="index" @close="index = null">
+      </CoolLightBox>
+
+      <div class="images-wrapper">
+        <div
+          class="image"
+          v-for="(image, imageIndex) in items"
+          :key="imageIndex"
+          @click="index = imageIndex"
+          :style="{ backgroundImage: 'url(' + image + ')' }"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import CoolLightBox from "vue-cool-lightbox";
 export default {
   data() {
     return {
@@ -55,8 +68,16 @@ export default {
         centerMode: true,
         focusOnSelect: true,
         arrows: false
-      }
+      },
+      items: [
+        "https://cosmos-images2.imgix.net/file/spina/photo/20565/191010_nature.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=835",
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1557340276.jpg?crop=0.666xw:1.00xh;0.168xw,0&resize=640:*"
+      ],
+      index: null
     };
+  },
+  components: {
+    CoolLightBox
   }
 };
 </script>
