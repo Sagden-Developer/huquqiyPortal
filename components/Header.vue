8f27883664e7b7f70785880a00f3b7aae80273be
AkmalNorqobilov <norqobilovakmal64397@gmail.com>
2021-06-23 11:36:00 +0500

    fsdlkfjldfj

--- components/Header.vue
+++ components/Header.vue
@@ -71,7 +71,6 @@
                 </svg>
                 <span>O'zbekcha</span>
                 <svg
-                  :class=""
                   xmlns="http://www.w3.org/2000/svg"
                   width="8"
                   height="6"
