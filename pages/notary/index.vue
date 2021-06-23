f192d72113a11f43617dc12af9dbb7c4354cd395
unknown <Abdulaziz2004>
2021-06-23 23:42:31 +0500

    add

---
+++ pages/notary/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+         <div class="boxs">
+      <div class="box"> 
+        <h4>Notariat</h4>
+        <p>
+          <nuxt-link style="color: white; text-decoration: none" to="/" href="#"> Bosh sahifa </nuxt-link> / Targ’ibot
+          materiallari  
+        </p>
+      </div>
+    </div>
+    </div>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style lang="scss">
+    .boxs {
+  background: #597ba3;
+  width: 1340px;
+  height: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 24px;
+  margin-top: 26px;
+  .box {
+    width: 1700px;
+    margin: 0 auto;
+    padding: 15px 0;
+    h4 {
+      font-size: 24px;
+      line-height: 29px;
+      margin-top: 33px;
+      color: #fff;
+      font-weight: 600;
+    }
+    p {
+      font-size: 20px;
+      line-height: 24px;
+      color: #fff;
+      margin-top: 40px;
+    }
+  }
+}
+</style>
+
